d4f079d73e025e6a0809082dd475467a6266b856
kritika-0 <110775276+kritika-0@users.noreply.github.com>
2023-11-26 12:44:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+function preload(){
+
+  mus= loadImage("https://i.postimg.cc/htsKCDhr/mus.png");
+}
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide()
+    poseNet=ml5.poseNet(video,modelLoaded);
+ poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
+
+}
+
+
+ function gotPoses(results)
+ {
+   if(results.length>0) 
+   {
+    console.log(results);
+    noseX = results[0].pose.nose.x-25;
+    noseY = results[0].pose.nose.y+10;
+   } 
+ }
+ 
+function takeSnapshot(){
+    save('filter.png');
+
+}
+
+function draw()
+{
+ image(video,0,0,300,300);
+ image(mus,noseX,noseY,50,30);
+}
